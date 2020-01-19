a8c20efc3ef88b30e4e62c10de336f0f56bc2fd6
yuta4j1 <kascado.ys10@gmail.com>
2020-01-19 12:45:44 +0900

    ぐるなびAPIアクセスキーの取得まで

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
 	fmt.Println("Test")
+	apiKey, err := readKey()
+	if err != nil {
+		fmt.Println("APIアクセスキーの取得に失敗しました。")
+	}
+	fmt.Println(apiKey)
+}
+
+func readKey() (string, error) {
+	f, err := os.Open("access-key.txt")
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	buf, err := ioutil.ReadAll(f)
+	return string(buf), err
 }
