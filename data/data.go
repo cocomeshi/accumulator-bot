789530159830dcb6c75e22d072fa98a0cd10ef8f
yuta4j1 <kascado.ys10@gmail.com>
2020-02-19 23:38:31 +0900

    google places API から取得するデータ構造に修正

--- data/data.go
+++ data/data.go
@@ -1,17 +1,29 @@
 package data
 
 type Restaurant struct {
-	Id         string  `json:"id"`
-	Name       string  `json:"name"`
-	Url        string  `json:"url"`
-	Address    string  `json:"address"`
-	Opentime   string  `json:"opentime"`
-	Categories string  `json:"category"`
-	Latitude   float64 `json:"latitude"`
-	Longitude  float64 `json:"longitude"`
+	Id           string       `json:"id"`
+	Name         string       `json:"name"`
+	Rating       float64      `json:"rating"`
+	Geometry     Geometry     `json:"geometry"`
+	Types        []string     `json:"types"`
+	OpeningHours OpeningHours `json:"opening_hours"`
+}
+
+type OpeningHours struct {
+	openNow bool `json:"open_now"`
+}
+
+type Location struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lng"`
+}
+
+type Geometry struct {
+	Location Location `json:"location"`
 }
 
 type RestaurantResponse struct {
-	HitNum      int8         `json:"hit_per_page"`
-	Restaurants []Restaurant `json:"rest"`
+	HitNum        int8         `json:"hit_per_page"`
+	NextPageToken string       `json:"next_page_token"`
+	Restaurants   []Restaurant `json:"results"`
 }
