c881b19bf56cce5a359db10c52d47417d203ccd7
yuta4j1 <kascado.ys10@gmail.com>
2020-04-07 23:50:13 +0900

    全Places ID検索処理追加

--- infrastructure/restaurant_repository.go
+++ infrastructure/restaurant_repository.go
@@ -9,6 +9,29 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// 現在登録されている全place IDの配列を返す
+func SelectIds(db *sql.DB) ([]string, error) {
+
+	rows, err := db.Query("select id from restaurant")
+	if err != nil {
+		return []string{}, err
+	}
+	defer rows.Close()
+
+	var res data.Restaurant
+	var idArray []string
+	for rows.Next() {
+		err := rows.Scan(&res.Id)
+		if err != nil {
+			fmt.Println(err)
+		}
+		idArray = append(idArray, res.Id)
+	}
+
+	return idArray, nil
+
+}
+
 func Insert(db *sql.DB, datas []data.Restaurant) {
 	tx, err := db.Begin()
 	if err != nil {
@@ -19,10 +42,9 @@ func Insert(db *sql.DB, datas []data.Restaurant) {
 		log.Fatal(err)
 	}
 	for _, d := range datas {
-		fmt.Println(d)
 		r, e := ins.Exec(d.Id, d.Name, "", "", "", d.Geometry.Location.Longitude, d.Geometry.Location.Latitude, "kinki")
 		if e != nil {
-			fmt.Println(e)
+			log.Fatal(e)
 		}
 		fmt.Println(r)
 	}
